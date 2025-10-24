70badba807eb6ffe7488b02462f44c8676903d71
kaiquemoreira7 <kaique.souza@germinare.org.br>
2025-10-24 15:29:40 -0300

    commit

--- app.py
+++ app.py
@@ -16,11 +16,6 @@ def load_model(filename='model.pkl'):
     with open(filename, 'rb') as f:
         return pickle.load(f)
 
-class InputData(BaseModel):
-    col1: int; col2: int; col3: int; col4: int; col5: int
-    col6: int; col7: int; col8: int; col9: int
-    col10: float; col11: float
-
 app = FastAPI()
 
 @app.get("/")
@@ -28,9 +23,8 @@ def home():
     return {"message": "API is Live"}
 
 @app.post("/predict")
-def predict(data: InputData):
-    X = [[data.col1, data.col2, data.col3, data.col4, data.col5,
-          data.col6, data.col7, data.col8, data.col9, data.col10, data.col11]]
+def predict(inputs: str):  # 1,0,1,0.82317...
+    X = [[float(x) if '.' in x else int(x) for x in inputs.split(',')]]
     y = model.predict(X)[0]
     return {"prediction": int(y)}
 
